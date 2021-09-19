c33e4ffe44382128585b9eec2bb40b606a6d56b6
Muhammad Khoirul Anam <mkhoir.anam@gmail.com>
2021-09-19 19:04:05 +0700

    image optimization

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,8 +1,10 @@
+import Image from 'next/image';
 import Layout from '../components/Layout';
 
 export default function Home() {
   return (
-    <Layout>
+    <Layout pageTitle="Home Page">
+      <Image src="/profile.png" width={200} height={200} alt="profile" />
       <h1 className="title">Welcome Anam</h1>
     </Layout>
   );
