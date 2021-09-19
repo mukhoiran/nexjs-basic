872df7110809b40b99c9bd67175193e28f72a581
Muhammad Khoirul Anam <mkhoir.anam@gmail.com>
2021-09-19 10:36:46 +0700

    custom 404

---
+++ pages/404.tsx
@@ -0,0 +1,8 @@
+export default function custom404() {
+  return (
+    <div>
+      <h1 className="title-not-found">Oooops..........</h1>
+      <h1 className="title-not-found">Page not found!</h1>
+    </div>
+  );
+}
