4402c715e4e5e5b218037dd0e98a31f052d6afd3
Muhammad Khoirul Anam <mkhoir.anam@gmail.com>
2021-09-19 10:39:22 +0700

    auto redirection

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,4 +1,15 @@
+import { useEffect } from 'react';
+import { useRouter } from 'next/router';
+
 export default function custom404() {
+  const router = useRouter();
+
+  useEffect(() => {
+    setTimeout(() => {
+      router.push('/');
+    }, 5000);
+  }, []);
+
   return (
     <div>
       <h1 className="title-not-found">Oooops..........</h1>
