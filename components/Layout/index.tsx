a9736e9f1042f31aa0ec5e3c8d73a933873e8860
Muhammad Khoirul Anam <mkhoir.anam@gmail.com>
2021-09-19 19:03:43 +0700

    create title and metadata

--- components/Layout/index.tsx
+++ components/Layout/index.tsx
@@ -5,14 +5,25 @@ import styles from './Layout.module.css';
 
 interface LayoutProps {
     children: ReactNode;
+    pageTitle: string;
 }
 export default function Layout(props: LayoutProps) {
-  const { children } = props;
+  const { children, pageTitle } = props;
   return (
-    <div className={styles.container}>
-      <Header />
-      <div className={styles.content}>{children}</div>
-      <Footer />
-    </div>
+    <>
+      <head>
+        <title>
+          NextJS Basic |
+          {' '}
+          {pageTitle}
+        </title>
+        <meta name="description" content="Website NextJS Basic" />
+      </head>
+      <div className={styles.container}>
+        <Header />
+        <div className={styles.content}>{children}</div>
+        <Footer />
+      </div>
+    </>
   );
 }
--- pages/blog.tsx
+++ pages/blog.tsx
@@ -2,7 +2,7 @@ import Layout from '../components/Layout';
 
 export default function Blog() {
   return (
-    <Layout>
+    <Layout pageTitle="Blog Page">
       <p className="title">This is blog</p>
     </Layout>
   );
